f6c3ed6cd2a100f36107245f555bc2206ad172c4
Pablo Ivulic <pablo@ivulic.com>
2018-08-17 23:50:16 +0200

    Output warning if there are uncommitted changes in hg or git

--- functions/bash-prompt.sh
+++ functions/bash-prompt.sh
@@ -2,9 +2,15 @@
 
 GREEN="\[\033[38;5;10m\]"
 BLUE="\[\033[38;5;33m\]"
+RED="\[\033[38;5;9m\]"
 YELLOW="\[\033[0;33m\]"
 NO_COLOR="\[\033[0m\]"
 
+function has-svc-changes() {
+    if [[ $(hg status 2> /dev/null) ]] || [[ $(git status -s 2> /dev/null) ]] ; then
+        echo "!"
+    fi
+}
 function get-git-branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
@@ -12,4 +18,4 @@ function get-hg-branch() {
     hg branch 2> /dev/null | sed -e "s/\(.*\)/ (\1)/"
 }
 
-export PS1="${GREEN}\u${NO_COLOR}:${BLUE}\w${YELLOW}\$(get-git-branch)\$(get-hg-branch)${NO_COLOR}\\$ "
+export PS1="${GREEN}\u${NO_COLOR}:${BLUE}\w${RED}\$(has-svc-changes)${YELLOW}\$(get-git-branch)\$(get-hg-branch)${NO_COLOR} $ "
