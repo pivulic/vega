408b0c63056ffa4f22bc68dde823011a257d9558
Pablo Ivulic <pablo@ivulic.com>
2019-07-15 18:49:03 +0200

    Adds alias to remove all Docker images

--- alias.sh
+++ alias.sh
@@ -9,16 +9,17 @@ alias d-composer='remove-container composer && docker run --interactive --tty --
 alias dangling-images='docker images -qf dangling=true'
 alias dangling-volumes='docker volume ls -qf dangling=true'
 alias docker-env='eval $(docker-machine env default)'
+alias ll='ls -la'
 alias m2='docker-compose exec web /var/www/html/bin/magento'
 alias m2-install-sampledata='docker-exec web sample-data'
 alias web-composer='docker-compose exec web composer --working-dir=/var/www/html'
 alias web-up='docker-compose up -d --build web'
 alias q='mkdir -p ~/projects && cd ~/projects'
 alias rm-all-containers=docker-rm-all-containers
+alias rm-all-images='docker rmi $(docker images -q)'
 alias rm-dangling-images='dangling-images | xargs docker rmi -f'
 alias rm-dangling-volumes='docker volume rm $(dangling-volumes)'
 alias ssh-docker=docker-exec
 alias ssh-k8=kubectl-exec
 alias ssh-key-copy='cat ~/.ssh/id_rsa.pub | pbcopy'
 alias vega-update='cd ~/vega && git pull && bash-reload'
-alias ll='ls -la'
