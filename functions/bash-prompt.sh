bbc44d1c103dc0374654ffbb31bfa9282644de9c
Pablo Ivulic <pablo@ivulic.com>
2022-03-02 08:38:07 +0100

    Show k8s context in bash prompt

--- functions/bash-prompt.sh
+++ functions/bash-prompt.sh
@@ -6,6 +6,20 @@ RED="\[\033[38;5;9m\]"
 YELLOW="\[\033[0;33m\]"
 NO_COLOR="\[\033[0m\]"
 
+function is-k8s-prod() {
+    CONTEXT=$(kubectl config current-context)
+    if [[ "$CONTEXT" == *"stocksholm_us"* ]]; then
+      echo "(k8s-prod)"
+    fi
+}
+
+function is-k8s-dev() {
+    CONTEXT=$(kubectl config current-context)
+    if [[ "$CONTEXT" == *"k8s-dev"* ]]; then
+      echo "(k8s-dev)"
+    fi
+}
+
 function has-svc-changes() {
     if [[ $(hg status 2> /dev/null) ]] || [[ $(git status -s 2> /dev/null) ]] ; then
         echo "!"
@@ -14,8 +28,5 @@ function has-svc-changes() {
 function get-git-branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-function get-hg-branch() {
-    hg branch 2> /dev/null | sed -e "s/\(.*\)/ (\1)/"
-}
 
-export PS1="${GREEN}\u${NO_COLOR}:${BLUE}\w${RED}\$(has-svc-changes)${YELLOW}\$(get-git-branch)\$(get-hg-branch)${NO_COLOR} $ "
+export PS1="${GREEN}\u\$(is-k8s-dev)${RED}\$(is-k8s-prod)${NO_COLOR}:${BLUE}\w${RED}\$(has-svc-changes)${YELLOW}\$(get-git-branch)${NO_COLOR} $ "
